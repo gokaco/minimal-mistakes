115f956ada1fb89c30533ac7f4808f36f1dc9007
gomaflasho <66081495+gomaflasho@users.noreply.github.com>
2020-10-16 23:32:50 +0530

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const pkg = require("./package.json");
-const filename = "assets/js/main.min.js";
-const script = fs.readFileSync(filename);
+const filename = "assets/js/main.mi.js";
+const script = fs.readFileSync(filename)
 const padStart = str => ("0" + str).slice(-2);
 const dateObj = new Date();
-const date = `${dateObj.getFullYear()}-${padStart(
+const date = `${dateObj.getFullYear()}-${padStart
   dateObj.getMonth() + 1
 )}-${padStart(dateObj.getDate())}`;
 const banner = `/*!
